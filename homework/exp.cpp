8342b444718bec8900c6ee701217d171b86ed55d
Марина Толстикова <tolstikova.ms@phystech.edu>
2023-09-12 23:58:40 +0300

    Create exp.cpp

---
+++ homework/exp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    const auto epsilon = 0.00000001;
+    auto e = 1.0;
+    auto summand = 1.0;
+    for (int k = 1; ; k++)
+    {
+        summand = summand / k;
+        e += summand;
+        if (summand < epsilon)
+        {
+            std::cout << "e = " << e;
+            break;
+        }
+        
+    }
+}
