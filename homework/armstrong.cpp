1f4ec6921dc0fdc93028812fa3716d29fbbef2ee
Марина Толстикова <tolstikova.ms@phystech.edu>
2023-09-12 21:56:34 +0300

    Create armstrong.cpp

---
+++ homework/armstrong.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    const auto maxnumber = 1000;
+    for (auto number = 100; number < 1000; number++) 
+    {
+        auto third_digit = number % 10;
+        auto second_digit = (number / 10) % 10; 
+        auto first_digit = number / 100;
+        //std::cout << first_digit << second_digit << third_digit << " \n";
+        if ((first_digit*first_digit*first_digit + second_digit*second_digit*second_digit + third_digit*third_digit*third_digit) == number)
+            {
+                std::cout << number <<" \n";
+                continue;
+            }
+    }     
+}
