c3e0f9263565c723485e606ea45ae2bed02316b5
Марина Толстикова <tolstikova.ms@phystech.edu>
2023-10-03 01:05:03 +0300

    Create covariant_return_type.cpp

---
+++ hw4/covariant_return_type.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+
+class Gun
+{
+public:
+    virtual void shoot()
+    {
+        std::cout << "Bang!" << std::endl;
+    }
+};
+
+
+class Machine_gun : public Gun
+{
+public:
+    void shoot() override
+    {
+        std::cout << "Bang! Bang! Bang!" << std::endl;
+    }
+};
+
+
+class Gun_factory
+{
+public:
+    virtual Gun* produce()
+    {
+        std::cout << "Gun is produced." << std::endl;
+        return new Gun;
+    }
+};
+
+
+class Machine_gun_factory : public Gun_factory
+{
+public:
+    Machine_gun* produce() override
+    {
+        std::cout << "Machine gun is produced." << std::endl;
+        return new Machine_gun;
+    }
+};
+
+
+int main()
+{
+    Machine_gun_factory mgf;
+    Gun_factory gf;
+
+    Gun* new_gun = gf.produce();
+    new_gun->shoot();
+
+    Machine_gun* new_machine_gun = mgf.produce();
+    new_machine_gun->shoot();
+
+    return 0;
+}
+
