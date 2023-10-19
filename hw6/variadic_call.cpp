f541fde3fe7ae4bd360850c21e4987a0861b917a
Марина Толстикова <tolstikova.ms@phystech.edu>
2023-10-20 02:25:08 +0300

    Update variadic_call.cpp

--- hw6/variadic_call.cpp
+++ hw6/variadic_call.cpp
@@ -3,7 +3,7 @@
 template<typename foo, typename... Types>
 void call_function(foo&& f, Types&&... args)
 {
-    f(std::forward<Types>(args)...);
+    f(std::move<Types>(args)...);
 }
 
 void sum(int a, int b)
