91f8d423f89a525aff26c821ab55e3c64f4270fc
Марина Толстикова <tolstikova.ms@phystech.edu>
2024-03-26 02:53:04 +0300

    Algorithms on ranges(2)

---
+++ 18. Algoritms on ranges (2)/12.08 [50] Algorithms on Ranges.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <vector>
+#include <ranges>
+
+void print(std::vector < int > & v)
+{
+    for(auto e:std::views::all(v)){std::cout << e << " ";};
+    std::cout << std::endl; 
+}
+
+
+int main()
+{
+    constexpr std::size_t size = 10; 
+
+    std::vector < int > v1(size);
+
+	std::iota(std::begin(v1), std::end(v1), 0); // v1 = 0 1 2 3 4 5 6 7 8 9 
+
+    std::ranges::for_each_n(std::begin(v1), 3, [](auto & n){n *= 7;}); // v1 = 0 7 14 3 4 5 6 7 8 9
+
+    assert(std::ranges::count_if(v1, [](auto n){return n % 7 == 0;}) == 4);
+
+    std::shift_left(std::begin(v1), std::end(v1), 3); // v1 = 3 4 5 6 7 8 9 7 8 9 
+
+    std::ranges::rotate(std::begin(v1), (std::begin(v1) + std::size(v1)/2), std::end(v1)); // v1 = 8 9 7 8 9 3 4 5 6 7
+
+    std::ranges::fill_n(std::begin(v1), 3, 1); // v1 = 1 1 1 8 9 3 4 5 6 7 
+
+    std::vector < int > v2;
+    std::ranges::unique_copy(v1, std::back_inserter(v2)); // v2 = 1 8 9 3 4 5 6 7 
+
+    std::ranges::replace_if(v2, [](auto n){return n % 3 == 0;}, 2); // v2 = 1 8 2 2 4 5 2 7
+
+    std::iter_swap(std::begin(v2), std::end(v2)- 1); //v2 = 7 8 2 2 4 5 2 1 
+
+    std::vector < int > v3(4);
+    std::ranges::copy_n(std::begin(v2), 4, std::begin(v3)); // v3 = 7 8 2 2
+
+    assert(*std::ranges::mismatch(v2, v3).in1 == 4); //first mismatching element in v2
+
+}
