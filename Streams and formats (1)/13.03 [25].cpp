387a70cbdf560dca980f4dbd92d891dd6458988a
Марина Толстикова <tolstikova.ms@phystech.edu>
2024-05-18 20:29:09 +0300

    corrected

--- Streams and formats (1)/13.03 [25].cpp
+++ Streams and formats (1)/13.03 [25].cpp
@@ -13,16 +13,16 @@ struct Process
     std::string user;
 };
 
-void output_processes(std::vector<Process> processes) 
+void output_processes(const std::vector<Process> & processes) 
 {
-    std::sort(std::begin(processes), std::end(processes), [](Process p1, Process p2){return p1.name < p2.name;});
+    std::sort(std::begin(processes), std::end(processes), [](const Process & p1, const Process & p2){return p1.name < p2.name;});
     std::cout << std::setw(25) << std::left << "Process Name";
     std::cout << std::setw(7) << std::left << "ID";
     std::cout << std::setw(10) << std::left << "Status";
     std::cout << std::setw(20) << std::left << "User";
     std::cout << '\n';
 
-    for (auto const p:processes)
+    for (auto const & p:processes)
     {
         std::cout << std::setw(25) << std::left << p.name;
         std::cout << std::setw(6) << std::right << p.id << ' ';
