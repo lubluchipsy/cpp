2eefff9b38c08cbca37d7036a93744c38c754667
Марина Толстикова <tolstikova.ms@phystech.edu>
2024-05-19 22:21:24 +0300

    corrected

--- Streams and formats (1)/13.01 [25].cpp
+++ Streams and formats (1)/13.01 [25].cpp
@@ -3,12 +3,13 @@
 #include <cassert>
 #include <sstream>
 #include <string>
+#include <iomanip>
 
-std::string ints_to_string(std::vector <int> nums)
+std::string ints_to_string(const std::vector <int> & nums)
 {
     std::ostringstream stream;
     stream.setf(std::ios_base::uppercase);
-    stream << std::hex;
+    stream << std::hex << std::noshowbase << std::setw(2);
     for (auto i:nums)
     {
         stream << i;
@@ -20,5 +21,6 @@ int main()
 {
     std::vector <int> numbers {0xBA, 0xAD};
     assert(ints_to_string(numbers) == "BAAD");
+    std::cout << ints_to_string(numbers);
     return 0;
 }
