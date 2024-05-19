156d30edbb64ce45fb5a3a43e45869108335eb6f
Марина Толстикова <tolstikova.ms@phystech.edu>
2024-05-19 22:06:01 +0300

    corrected

--- 13. Text Data Processing (2)/13.12 [25] Regexes (4).cpp
+++ 13. Text Data Processing (2)/13.12 [25] Regexes (4).cpp
@@ -3,15 +3,16 @@
 #include <regex>
 #include <string>
 #include <cassert>
+#include <string_view>
 
 std::string transform_date(std::string_view text)
 {
-    auto rx = std::regex{ R"((\d{1,2})(\.|-|/)(\d{1,2})(\.|-|/)(\d{4}))" };
-    return std::regex_replace(text.data(), rx, R"($5-$3-$1)");
+    auto rx = std::regex{ R"(([0-1][0-9])(\/)([0-3][0-9])(\/)(\d{4}))" };
+    return std::regex_replace(text.data(), rx, R"($3.$1.$5)");
 }
 int main()
 {
     using namespace std::string_literals;
-    assert(transform_date("today is 01.12.2017!"s) == "today is 2017-12-01!"s);
+    assert(transform_date("today is 12/01/2017!"sv) == "today is 01.12.2017!"sv);
     return 0;
 }
